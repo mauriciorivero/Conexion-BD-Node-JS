885652a932cb5c0a6865d0ccce676f4edee8b02d
mauriciorivero <rivero.mauricio@gmail.com>
2025-11-26 07:09:58 -0500

    Update database connection configuration
    Replaced hardcoded database connection details with environment variables.

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Configuración de la conexión a la base de datos
 const db = mysql.createConnection({
-    host: 'localhost',
-    port: 8889,
-    user: 'mauro',
-    password: 'm@ur0_1234*',
-    database: 'carniceria'
+    host: DB_HOST, //la ip o nombre de dominio donde esta alojado el motor de bases de datos
+    port: DB_PORT, //El puerto donde esta escuchando el motor de bases de datos
+    user: DB_USER, //El usuario con permisos concedidos para tomar acciones sobre la base de datos
+    password: DB_PASS, //La contraseña del usuario con permisos a la BD
+    database: DB_NAME //El nombre de la base de datos sobre la cual se va a trabajar
 });
 
 // Conectar a la base de datos
